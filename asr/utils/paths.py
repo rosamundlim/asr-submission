34ed871786a8a23310134fc07750d688fe6ae69d
Rosamund Lim Shi Ni <rosamund_lim_sn@aiap.sg>
2024-12-19 18:07:35 +0800

    updated with comments to better reflect subdirectories

--- asr/utils/paths.py
+++ asr/utils/paths.py
@@ -6,11 +6,15 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # ./asr/ directory
+# Folders
 ASR = BASE_DIR / "asr"
 CV_VALID_DEV = ASR / "cv-valid-dev"
 MODEL_ARTIFACTS = ASR / "model_artifacts"
 CONF = ASR / "conf"
+# Files
+CV_VALID_DEV_CSV = ASR / "cv-valid-dev.csv"
 
 # ./CONF/ directory
+# Files
 OPENAPI = CONF / "openapi.yml"
 CONFIG = CONF / "config.yml"
