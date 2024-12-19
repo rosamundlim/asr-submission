99abf7fb1de79dddef2a5ce866c51064c66c1778
Rosamund Lim Shi Ni <rosamund_lim_sn@aiap.sg>
2024-12-20 01:05:32 +0800

    Completed code for calling asr API to inference .mp3 files

--- asr/cv-decode.py
+++ asr/cv-decode.py
@@ -1,31 +1,37 @@
 """
 Task 2d - to transcribe all 4,076 .mp3 files in cv-valid-dev (Common Voice dataset).
-References: 
-i. aiohttp/asyncio documentation, 
+References:
+i. aiohttp/asyncio documentation,
 ii. https://www.twilio.com/en-us/blog/asynchronous-http-requests-in-python-with-aiohttp
 iii. openai chatgpt 3.5
 """
 
 import os
 import time
+from datetime import datetime
+import csv
 import logging
 import asyncio
 from typing import List
 import aiofiles
 import aiohttp
 from tqdm.asyncio import tqdm
-import pandas as pd
 from utils import paths, utility_functions
-
-logging.basicConfig(level=logging.INFO,format='%(asctime)s:%(levelname)s:%(message)s')
 
 # Define paths and variables
 AUDIO_DIRECTORY = paths.CV_VALID_DEV
-# RESULTS_CSV_PATH = paths.CV_VALID_DEV_CSV
-RESULTS_CSV_PATH = paths.ASR
+RESULTS_CSV_PATH = paths.CV_VALID_DEV_CSV
 CONFIG_FILE = utility_functions.load_yaml(paths.CONFIG)
 INFERENCE_URL = CONFIG_FILE['asr_api']['inference']
 PING_URL = CONFIG_FILE['asr_api']['ping']
+TCP_LIMIT = CONFIG_FILE['cv-decode']['aiohttp_tcp_connectors']
+TIMEOUT_LIMIT = CONFIG_FILE['cv-decode']['aiohttp_timeout']
+SEMAPHORE_LIMIT = CONFIG_FILE['cv-decode']['semaphore_limit']
+
+# Set logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s:%(levelname)s:%(message)s'
+                    )
 
 class AudioTranscriber:
     """
@@ -35,22 +41,24 @@ class AudioTranscriber:
                  audio_dir: str,
                  results_path: str,
                  inference_url: str,
-                 health_url: str
+                 health_url: str,
+                 tcp_limit: int = TCP_LIMIT,
+                 timeout_limit: int = TIMEOUT_LIMIT,
+                 semaphore_limit: int = SEMAPHORE_LIMIT
     ):
         if not os.path.isdir(audio_dir):
             raise NotADirectoryError(f"Not a valid directory: {audio_dir}")
-        #if not os.path.isfile(results_path):
-        #    raise FileNotFoundError(f"File not found: {results_path}")
+        if not os.path.isfile(results_path):
+            raise FileNotFoundError(f"File not found: {results_path}")
         self.audio_dir = audio_dir
+        self.results_path = results_path
         self.mp3_filenames = None
         self.check_valid_audio()
-        self.results_path = results_path
         self.inference_url = inference_url
         self.health_url = health_url
-        self.transcription = {'generated_text': [],
-                              'duration': [],
-                              'audiofile': []
-                              }
+        self.tcp_limit = tcp_limit
+        self.timeout_limit = timeout_limit
+        self.semaphore_limit = semaphore_limit
 
     def check_valid_audio(self) -> None:
         """
@@ -60,7 +68,21 @@ class AudioTranscriber:
         audio_dir_files = os.listdir(self.audio_dir)
         self.mp3_filenames = [f for f in audio_dir_files if f.endswith('.mp3')]
         logging.info("Found %s .mp3 files.", len(self.mp3_filenames))
- 
+        required_files = set()
+
+        with open(self.results_path, 'r', encoding='utf-8') as infile:
+            reader = csv.reader(infile)
+            _ = next(reader) #skip header
+            for row in reader:
+                filename_col = row[0]
+                extracted_filename = os.path.basename(filename_col)
+                required_files.add(extracted_filename)
+
+        missing_filenames = required_files - set(self.mp3_filenames)
+        if missing_filenames:
+            raise ValueError(f"Missing .mp3 files: {missing_filenames}")
+        logging.info("No missing .mp3 files")
+
     async def health_check(self, session: aiohttp.ClientSession) -> None:
         """
         Makes get request for health check
@@ -78,7 +100,6 @@ class AudioTranscriber:
         """
         pass
         """
-        start_time = time.perf_counter()
         audio_filepath = os.path.join(self.audio_dir, filename)
         form = aiohttp.FormData()
 
@@ -92,62 +113,77 @@ class AudioTranscriber:
                 return {
                     'generated_text': f'Error: {resp.status}',
                     'duration': f'Error: {resp.status}',
-                    'audiofile': filename,
                 }
+
             response = await resp.json()
-            
-            # to remove later
-            end_time = time.perf_counter()
-            elapsed_time = end_time - start_time
-            
-            logging.info(f"Time taken for {filename}: {elapsed_time:.2f} seconds")
-            
+
             return {
                 'generated_text': response['transcription'],
                 'duration': response['duration'],
-                'audiofile': filename
             }
 
     async def transcribe_files_in_dir(self, session: aiohttp.ClientSession):
         """
         pass
         """
+        semaphore = asyncio.Semaphore(self.semaphore_limit)
         tasks = []
-        for file in self.mp3_filenames[:50]:
-            tasks.append(asyncio.ensure_future(self.transcribe_single_audio(session, \
-                                                                            filename=file)))
-        results = await asyncio.gather(*tasks)
-        #results = await tqdm.gather(*tasks, total=len(tasks), desc='Transcribing .mp3 files')
+
+        async def transcribe_with_limit(file):
+            async with semaphore:
+                return await self.transcribe_single_audio(session, filename=file)
+
+        for file in self.mp3_filenames:
+            tasks.append(asyncio.ensure_future(transcribe_with_limit(file)))
+
+        results = await tqdm.gather(*tasks, total=len(tasks), desc='Transcribing .mp3 files')
+        logging.info("Completed %s transcription tasks", len(tasks))
 
         return results
-    
+
     def write_results(self, results: List[dict[str, str]]):
-        for response in results:
-            self.transcription['generated_text'].append(response['generated_text'])
-            self.transcription['duration'].append(response['duration'])
-            self.transcription['audiofile'].append(response['audiofile'])
-        df = pd.DataFrame.from_dict(self.transcription)
-        filepath = os.path.join(self.results_path, 'transcription_results.csv')
-        df.to_csv(filepath, index=False)
+        parent_dir, filename = os.path.split(self.results_path)
+        temp_path = os.path.join(parent_dir, filename + '_temp')
+
+        logging.info('Writing results to: %s', self.results_path)
+        with open(self.results_path, 'r', encoding='utf-8') as infile, \
+            open(temp_path, 'w', encoding='utf-8', newline='') as outfile:
+
+            reader = csv.reader(infile)
+            writer = csv.writer(outfile)
+            header = next(reader)
+            header.extend(['generated_text', 'duration'])
+            writer.writerow(header)
+
+            for row, result in zip(reader, results):
+                if row:
+                    row.extend([result.get('generated_text', 'Null'),
+                                result.get('duration','Null')]
+                                )
+                    writer.writerow(row)
+        os.replace(temp_path, self.results_path)
 
     async def main(self):
         """
         pass
         """
-        async with aiohttp.ClientSession() as session:
+        connector = aiohttp.TCPConnector(limit=self.tcp_limit)
+        timeout = aiohttp.ClientTimeout(total=self.timeout_limit)
+
+        async with aiohttp.ClientSession(connector=connector, timeout=timeout) as session:
             await self.health_check(session)
             results = await self.transcribe_files_in_dir(session)
-        
+
         self.write_results(results)
 
 if __name__ == "__main__":
-    #start_time = time.perf_counter()
+    start_time = time.perf_counter()
     transcriber = AudioTranscriber(audio_dir=AUDIO_DIRECTORY,
                                    results_path=RESULTS_CSV_PATH,
                                    inference_url=INFERENCE_URL,
                                    health_url=PING_URL,
                                    )
     asyncio.run(transcriber.main())
-    #end_time = time.perf_counter()
-    #elapsed_time = end_time - start_time
-    #logging.info('Program Duration: %.2f seconds', elapsed_time)
+    end_time = time.perf_counter()
+    elapsed_time = end_time - start_time
+    logging.info('Program Duration: %.2f seconds', elapsed_time)
